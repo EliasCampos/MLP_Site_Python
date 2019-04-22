e9e4949130c1011dfff43fcf0857e85a872fb84d
Elias <camposylia@gmail.com>
2019-04-22 16:49:34 +0300

    Described next model classes: Status,Tag.

--- backend/projectApp/models.py
+++ backend/projectApp/models.py
@@ -7,8 +7,50 @@ class Project(models.Model):
 
 
 class Tag(models.Model):
-    pass
+    """ Represents tag which marks programming language or technology,
+        used in some project. Project can have many tags, so it has
+        one to many relation with Project model. """
 
+    title = models.CharField(
+        _('title'),
+        max_length=50,
+        unique=True,
+        help_text="At most 50 characters."
+    )
+    slug = models.SlugField(
+        _('slug'),
+        max_length=50,
+        unique=True,
+        help_text="At most 50 characters, allowed characters:'-_a-z0-9'."
+    )
+
+    class Meta:
+        verbose_name = "project's tag"
+        verbose_name_plural = "project's tags"
+
+    def save(self, *args, **kwargs):
+        """ Before storing data in database,
+        converts slug string to lower case. """
+
+        self.slug = self.slug.lower()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
 
 class Status(models.Model):
-    pass
+    """ Represents status of some project. (for ex. 'active', 'complete') """
+
+    title = models.CharField(
+        _('title'),
+        max_length=16,
+        unique=True,
+        help_text="At most 16 characters."
+    )
+
+    class Meta:
+        verbose_name = "project's status"
+        verbose_name_plural = "project's statuses"
+
+    def __str__(self):
+        return "Status: '%s'." % self.title
