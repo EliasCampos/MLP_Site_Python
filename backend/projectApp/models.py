9b2805a4b051f910378b1ee07ba0ec5ed1865e35
Elias <camposylia@gmail.com>
2019-04-24 22:13:02 +0300

    Implemented Project model, added 'tinymce' (html WYSIWYG)

--- backend/projectApp/models.py
+++ backend/projectApp/models.py
@@ -1,10 +1,87 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from djang.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
+
+from tinymce.models import HTMLField
 
 
 class Project(models.Model):
-    pass
+    """
+    Represents project, published on the web-site.
+    """
 
+    title = models.CharField(
+        _('title'),
+        max_length=255,
+        unique=True,
+        help_text="At most 255 characters."
+    )
+    slug = models.SlugField(
+        _('slug'),
+        max_length=128,
+        unique=True,
+        help_text="At most 128 characters, allowed characters:'-_a-z0-9'."
+    )
+    preview = models.ImageField(
+        _('preview'),
+        upload_to='projectApp/previews/',
+        blank=True,
+        null=True
+    )
+    short_description = models.TextField(_('short description'))
+    full_description = models.HTMLField(_('full description'))
+    number_of_people = models.PositiveSmallIntegerField(
+        _('number of people'),
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(1000)],
+        help_text="Positive integer in range 1 to 1000 inclusive."
+    )
+    date_of_created = models.DateTimeField(
+        _('date of creation'),
+        auto_now_add=True
+    )
+    date_of_updated = models.DateTimeField(
+        _('date of update'),
+        auto_now=True
+    )
+    date_of_end = models.DateTimeField(_('date of end'))
+    is_active = models.BooleanFiel(_('is active'), default=True)
+
+    status = models.ForeignKey(
+        'Status',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        verbose_name=_('status')
+    )
+    tags = models.ManyToManyField(
+        'Tag',
+        related_name='projects',
+        verbose_name=_('tags')
+    )
+
+    class Meta:
+        verbose_name = "enrolled project"
+        verbose_name_plural = "enrolled projects"
+
+    def save(self, *args, **kwargs):
+        # Update slug field, if it's empty - write slugified title there:
+        self.slug = self.slug.lower() if self.slug else slugify(self.title)
+
+        # Check preview, if it has updated, remove old image:
+        try:
+            old_self = Project.objects.get(id=self.id)
+            if old_self.preview != self.preview:
+                old_self.preview.delete(save=False)
+        except:
+            # If it's a new project or a first preview, just don't do nothing:
+            pass
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "'%s' project." % self.title
 
 class Tag(models.Model):
     """ Represents tag which marks programming language or technology,
