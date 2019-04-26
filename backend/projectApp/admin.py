9bace92a473cc6641995fe00253f44d014cf3f2c
AlTheOne <AlTheOne.official@gmail.com>
2019-04-26 17:57:43 +0300

    Add_fix_Add_settings

--- backend/projectApp/admin.py
+++ backend/projectApp/admin.py
@@ -5,10 +5,8 @@ from projectApp.models import Project, Tag, Status
 class ProjectAdmin(admin.ModelAdmin):
     pass
 
-
 class TagAdmin(admin.ModelAdmin):
     pass
-
 
 class StatusAdmin(admin.ModelAdmin):
     pass
