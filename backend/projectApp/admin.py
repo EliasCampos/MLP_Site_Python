169c40b4ad9973d6648ae9a7639e733ddba0c96a
Elias <camposylia@gmail.com>
2019-04-27 14:39:24 +0300

    Finished admin for project app.

--- backend/projectApp/admin.py
+++ backend/projectApp/admin.py
@@ -3,7 +3,28 @@ from projectApp.models import Project, Tag, Status
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        'id',
+        'title',
+        'status',
+        'is_active',
+        'date_of_updated',
+        'date_of_end',
+        'date_of_created'
+    )
+    list_display_links = ('title',)
+    list_filter = ('status', 'is_active')
+    readonly_fields = ('date_of_updated', 'date_of_created')
+    fields = (
+        'title',
+        'slug',
+        'preview',
+        ('short_description', 'full_description'),
+        'number_of_people',
+        'is_active',
+        ('date_of_updated', 'date_of_end', 'date_of_created')
+    )
+    save_on_top = True
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
